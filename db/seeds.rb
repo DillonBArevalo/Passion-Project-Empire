47cd9ccbc7a10833b9c3d4543943ef2ae8fb7a49
Dillon Arevalo <dillonbarevalo@gmail.com>
2017-04-02 12:47:08 -0700

    Create seeds file and populate weapon types

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+WeaponType.create(name: "Short Sword", range: 1, hands_required: 1)
+WeaponType.create(name: "Sword", range: 1, hands_required: 1)
+WeaponType.create(name: "Great Sword", range: 1, hands_required: 2)
+WeaponType.create(name: "Small Axe", range: 1, hands_required: 1)
+WeaponType.create(name: "Large Axe", range: 1, hands_required: 2)
+WeaponType.create(name: "Mace", range: 1, hands_required: 1)
+WeaponType.create(name: "Maul", range: 1, hands_required: 2)
+WeaponType.create(name: "Spear", range: 2, hands_required: 2)
+WeaponType.create(name: "Short Spear", range: 1, hands_required: 1)
+WeaponType.create(name: "Halberd", range: 2, hands_required: 2)
+WeaponType.create(name: "Pole Axe", range: 2, hands_required: 2)
+
+
