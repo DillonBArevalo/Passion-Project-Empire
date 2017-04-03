a7c86383818548cc3e603884fa1f8af0c5485b65
Dillon Arevalo <dillonbarevalo@gmail.com>
2017-04-02 17:35:31 -0700

    Add empty restful routes for users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+get '/users' do
+  #all users
+end
+
+get '/users/new' do
+
+end
+
+post '/users' do
+
+end
+
+get '/users/:id' do
+
+end
+
+get '/users/:id/edit' do
+
+end
+
+put '/users/:id' do
+
+end
+
+delete '/users/:id' do
+
+end
