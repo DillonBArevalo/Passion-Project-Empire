c8a73ee56557f68b0d083ff10e90b3d3bcb807c4
Dillon Arevalo <dillonbarevalo@gmail.com>
2017-04-05 20:02:12 -0700

    Begin process of adding weapons

--- app/controllers/weapons_controller.rb
+++ app/controllers/weapons_controller.rb
@@ -1,4 +1,9 @@
 get '/weapons' do
-  @weapons = Weapons.all
+  ### MAKE MIGRATION TO ADD CREATOR ID TO WEAPONS BEFORE THIS WILL WORK
+  # @weapons = Weapon.all
+  # @base = Weapon.where(creator_id: nil)
+  # @user_weapons = Weapon.where("creator_id > 0")
+  # p @base
+  # p @user_weapons
   erb :'weapons/index'
 end
