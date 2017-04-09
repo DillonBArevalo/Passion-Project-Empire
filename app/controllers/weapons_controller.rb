3f5d19b0f0c45124333c1e6e80615036f576358f
Dillon Arevalo <dillonbarevalo@gmail.com>
2017-04-08 19:23:21 -0700

    Add weapons index with route (not currently linked to anywhere) that shows weapon cards

--- app/controllers/weapons_controller.rb
+++ app/controllers/weapons_controller.rb
@@ -1,9 +1,4 @@
 get '/weapons' do
-  ### MAKE MIGRATION TO ADD CREATOR ID TO WEAPONS BEFORE THIS WILL WORK
-  # @weapons = Weapon.all
-  # @base = Weapon.where(creator_id: nil)
-  # @user_weapons = Weapon.where("creator_id > 0")
-  # p @base
-  # p @user_weapons
+  @weapons = Weapon.all
   erb :'weapons/index'
 end
